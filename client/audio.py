30147a2b663fda58038578578b038af86b75fa07
灰 <messenger@siyfico.cloud>
2022-02-07 02:54:23 +0800

    Update audio.py

--- client/audio.py
+++ client/audio.py
@@ -2,7 +2,7 @@ from pydub import AudioSegment
 
 
 def clip(path, pos, output_filename):
-    def sec(t):
+    def sec2ms(t):
         return t * 1000
     audio = AudioSegment.from_mp3(path)
-    audio[sec(pos):].export(output_filename, format = "wav")
+    audio[sec2ms(pos):].export(output_filename, format = "wav")
